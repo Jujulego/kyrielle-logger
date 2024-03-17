a87438688ceef8a4550737e6f82df1890b037de7
Julien Capellari <julien.capellari@gmail.com>
2024-03-17 12:39:47 +0100

    chore: fix debug filter typings

--- src/filters/debug.ts
+++ src/filters/debug.ts
@@ -10,16 +10,16 @@ import { Log, LogLevel } from '../defs/index.js';
  * DEBUG should contain a comma separated list of labels. Logs with debug level will be filtered if their labels
  * are not in the DEBUG list. If DEBUG contains a wildcard '*' item, then no logs will be filtered.
  */
-export function envDebugFilter(debug = process.env.DEBUG) {
+export function envDebugFilter<L extends Log>(debug = process.env.DEBUG) {
   const filters = new Set(debug?.split(','));
 
   // Global wildcard
   if (filters.has('*')) {
-    return filter$<Subscribable<Log>>(() => true);
+    return filter$<Subscribable<L>>(() => true);
   }
 
   // Per label
-  return filter$<Subscribable<Log>>((log) => {
+  return filter$<Subscribable<L>>((log) => {
     if (log.level > LogLevel.debug || !hasLabel(log)) {
       return true;
     }
